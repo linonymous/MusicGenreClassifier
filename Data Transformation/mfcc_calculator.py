60188d11f3c02b35e3a0194889116f5887dc502c
Swapnil.Walke <Swapnil.Walke@veritas.com>
2018-01-05 09:57:19 +0530

    - add working code for following characteristic calculation - mfcc - spectral roll off - spectral centroid - zero crossing rate - chroma frequency

--- Data Transformation/mfcc_calculator.py
+++ Data Transformation/mfcc_calculator.py
@@ -5,4 +5,22 @@ wavData = AudioFile.open("C:\\Users\\Swapnil.Walke\\Downloads\\genres.tar\\genre
 fixedFrames = wavData.frames(1024)
 spectra = [f.spectrum() for f in fixedFrames]
 a = spectra[0].mfcc2(13)
+print "MFCC calculations"
 print a
+print "**************************************************"
+b= spectra[0].rolloff()
+print "spectral roll off"
+print b
+c = spectra[0].chroma()
+print "Chroma frequency"
+print c
+print len(c)
+d = spectra[0].centroid()
+print "Spectral Centroid"
+print d
+"""done with following 5 characteristics, but need to look upon how to calculate them all for one whole file
+- mfcc
+- spectral centroid
+- spectral roll off
+- chroma frequency
+- zero crossing rate"""
